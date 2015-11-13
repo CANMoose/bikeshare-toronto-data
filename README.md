96f85917c3a2e1aa42b65b0557553607b5744e0a
CANMoose <remphys@gmail.com>
2015-11-12 23:40:21 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Bike Share Toronto Data Analysis
+
+## Summary
+
+This is a new short project I have been working on where I want to try and datamine some interesting statistics about Bike Share Toronto. One thing I have in mind is to see which stations are most likely to be full or empty and at what times. 
+
+The code to grab and access live data is now running. Next step is to begin building a dataset over the course of at least a full week.
+
+ 
---
+++ data_analysis.py
@@ -0,0 +1,65 @@
+import urllib
+import xml.etree.ElementTree as ET
+import numpy as np
+from time import asctime
+
+class BikeBay(object):
+
+    def __init__(self, bayid, street, terminalname, lastcomm, lat, lon, installed, locked,\
+        temporary, public, nbikes, nempty, lastupdate):
+        self.id = bayid
+        self.street = street
+        self.terminalname = terminalname
+        self.lastcomm = lastcomm
+        self.lat = lat
+        self.long = lon
+        self.installed = installed
+        self.locked = locked
+        self.temporary = temporary
+        self.public = public
+        self.nbikes = nbikes
+        self.nempty = nempty
+        self.lastupdate = lastupdate
+
+######
+def getBikeBayData(url=True, fl=None):
+    '''Acesses the Toronto Bike Share bike bay data file and returns a list of bike
+    bay objects each with the information regarding each bay'''
+
+    if url:
+        url = 'http://www.bikesharetoronto.com/data/stations/bikeStations.xml'
+        data = urllib.urlopen(url).read()
+    
+        # Make record of data
+        f = open('/Users/relliotmeyer/gitrepos/bikeshare-toronto-data/data/'+asctime()+'.txt','w')
+        f.write(data)
+        f.close()
+    
+    elif fl:
+        flopen = open(fl,'r')
+        data = flopen.read()
+
+    root = ET.fromstring(data)
+
+    bikebays = []
+
+    for i, st in enumerate(root):
+        bikebays.append(BikeBay(st[0].text,st[1].text,st[2].text,st[3].text,\
+            st[4].text,st[5].text,st[6].text,st[7].text,st[10].text,st[11].text,\
+            st[12].text,st[13].text,st[14].text))
+        
+    return bikebays
+
+def check_updated(last):
+
+    new_data = getBikeBayData()
+
+    for bay in new_data:
+        pass
+        
+
+
+if __name__ == '__main__':
+
+    root = getBikeBayData()
+
